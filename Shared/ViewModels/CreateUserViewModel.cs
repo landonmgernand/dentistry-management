6d6cea3ea26ba85d9152cd46637c6fc28704414f
rashfordcholston <carltongstale@gmail.com>
2020-11-02 15:38:55 +0100

    Progress on design

--- Shared/ViewModels/CreateUserViewModel.cs
+++ Shared/ViewModels/CreateUserViewModel.cs
@@ -9,19 +9,23 @@ namespace DentistryManagement.Shared.ViewModels
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string PasswordHash { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
 }
