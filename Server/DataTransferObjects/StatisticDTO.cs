cf90774ccbae684d14178f0b61c0fcf640dd6bd6
rashfordcholston <carltongstale@gmail.com>
2020-12-12 10:16:33 +0100

    Implemented monthly and overall earnings statistic

---
+++ Server/DataTransferObjects/StatisticDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace DentistryManagement.Server.DataTransferObjects
+{
+    public class StatisticDTO
+    {
+        public int PatientCount { get; set; }
+
+        public int TreatmentCount { get; set; }
+
+        public decimal MonthlyEarnings { get; set; }
+
+        public decimal OverallEarnings { get; set; }
+
+        public MonthlyTreatmentHistoryDTO MonthlyTreatmentHistory { get; set; }
+
+        public PopularTreatmentDTO PopularTreatment { get; set; }
+    }
+}
